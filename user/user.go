7b039bcc0071af34c614d7c04d5032b474279ff0
Mel Savich <mksavic@nyu.edu>
2018-11-05 16:19:10 -0500

    added ok

--- user/user.go
+++ user/user.go
@@ -11,23 +11,23 @@ type User struct {
 }
 
 // create assigns a uuid to a username
-func (user *User) create(username string) uuid.UUID {
+func (user *User) create(username string) (uuid.UUID, ok bool) {
 	uuid := uuid.New()
 	users[uuid] = username
-	return uuid
+	return uuid, true
 }
 
 // view returns a uuid's corresponding username
-func (user *User) view(uuid uuid.UUID) (username string) {
-	return users[uuid]
+func (user *User) view(uuid uuid.UUID) (username string, ok bool) {
+	return users[uuid], true
 }
 
 // search returns a list of uuids that match a given query
-func (user *User) search(query string) (uuids []uuid.UUID) {
+func (user *User) search(query string) (uuids []uuid.UUID, ok bool) {
 	for uuid, username := range users {
 		if strings.Contains(username, query) {
 			uuids = append(uuids, uuid)
 		}
 	}
-	return uuids
+	return uuids, true
 }
