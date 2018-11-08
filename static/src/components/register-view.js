722d45ab887c016cf174eb9013c91150d242a75f
Miguel Salcedo <mds796@nyu.edu>
2018-11-07 23:23:19 -0500

    Create register view

--- static/src/components/register-view.js
+++ static/src/components/register-view.js
@@ -1,31 +1,54 @@
 import { html, PolymerElement } from '@polymer/polymer';
 
-import { ViewStyle } from './view-style.js';
+import {ViewStyle} from './view-style.js';
 
 /**
  * @customElement
  * @polymer
  */
 class RegisterView extends PolymerElement {
-  static get template() {
-    return html`
+    static get template() {
+        return html`
       <dom-if if=[[active]]>
         <template>
           ${ViewStyle}
-  
+
+          <style>
+            :host {
+              text-align: center
+            }
+          </style>
+
           <section>
-            <h2>Register view!</h2>
+            <h2>Register</h2>
+           
+            <form action="/register" method="post">
+              <div>
+                <label for="username">Username: </label>
+                <input id="username" name="username" placeholder="Username"/>
+              </div>
+              <div>
+                <label for="password">Password: </label>
+                <input id="password" name="password" type="password" placeholder="Password"/>          
+              </div>
+              <div>
+                <label for="password2">Re-enter Password: </label>
+                <input id="password2" name="password2" type="password" placeholder="Password"/>          
+              </div>
+                
+              <input type="submit" value="Register"/>
+            </form>
           </section>
         </template>
       </dom-if>
     `;
-  }
+    }
 
-  static get properties() {
-    return {
-      active: {type: Boolean, value: false}
-    };
-  }
+    static get properties() {
+        return {
+            active: {type: Boolean, value: false}
+        };
+    }
 }
 
 window.customElements.define('register-view', RegisterView);
