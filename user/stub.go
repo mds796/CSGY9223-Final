2cdbe5cc7fbc85838ba1f1ecd99b988ed9615ec5
Mel Savich <mksavic@nyu.edu>
2018-11-08 11:53:38 -0500

    user service now has two caches; one whose key is UUID, the other whose key is username

--- user/stub.go
+++ user/stub.go
@@ -7,38 +7,38 @@ import (
 )
 
 type StubService struct {
-	UsersCache map[string]string
+	UuidCache     map[string]string // (UUID, username)
+	UsernameCache map[string]string // (username, UUID)
 }
 
 func CreateStub() Service {
 	stub := new(StubService)
-	stub.UsersCache = make(map[string]string)
+	stub.UuidCache = make(map[string]string)
+	stub.UsernameCache = make(map[string]string)
 	return stub
 }
 
 func (s *StubService) Create(request CreateUserRequest) (CreateUserResponse, error) {
 	// ensure this username doesn't already exist
-	for _, username := range s.UsersCache {
-		if username == request.Username {
-			err := errors.New("[USER]: Username already exists.")
-			return CreateUserResponse{}, err
-		}
+	if _, ok := s.UsernameCache[request.Username]; ok {
+		err := errors.New("[USER]: Username already exists.")
+		return CreateUserResponse{}, err
 	}
 
 	// generate the uuid
-	user_uuid := uuid.New().String()
+	new_uuid := uuid.New().String()
 
 	// add the user
-	s.UsersCache[user_uuid] = request.Username
+	s.UuidCache[new_uuid] = request.Username
+	s.UsernameCache[request.Username] = new_uuid
 
 	// create the response
-	response := CreateUserResponse{Uuid: user_uuid}
-
+	response := CreateUserResponse{Uuid: new_uuid}
 	return response, nil
 }
 
 func (s *StubService) View(request ViewUserRequest) (ViewUserResponse, error) {
-	if username, ok := s.UsersCache[request.Uuid]; ok {
+	if username, ok := s.UuidCache[request.Uuid]; ok {
 		// uuid exists
 		response := ViewUserResponse{Username: username}
 		return response, nil
@@ -52,7 +52,7 @@ func (s *StubService) View(request ViewUserRequest) (ViewUserResponse, error) {
 func (s *StubService) Search(request SearchUserRequest) (SearchUserResponse, error) {
 	// find uuids that match given query
 	var uuids []string
-	for uuid, username := range s.UsersCache {
+	for uuid, username := range s.UuidCache {
 		if strings.Contains(username, request.Query) {
 			uuids = append(uuids, uuid)
 		}
@@ -60,6 +60,5 @@ func (s *StubService) Search(request SearchUserRequest) (SearchUserResponse, err
 
 	// create the response
 	response := SearchUserResponse{Uuids: uuids}
-
 	return response, nil
 }
