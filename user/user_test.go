08e7c50c859f7e9c3edd31fb2588f73bcb21eef1
Mel Savich <mksavic@nyu.edu>
2018-11-08 11:38:08 -0500

    user tests for search now check the contents of the response

--- user/user_test.go
+++ user/user_test.go
@@ -4,8 +4,18 @@ import (
 	"testing"
 )
 
+func contains(s []string, e string) bool {
+	for _, val := range s {
+		if val == e {
+			return true
+		}
+	}
+	return false
+}
+
 func TestUserCreateStandard(t *testing.T) {
 	service := CreateStub()
+
 	request := CreateUserRequest{Username: "mksavic"}
 	_, err := service.Create(request)
 
@@ -16,8 +26,10 @@ func TestUserCreateStandard(t *testing.T) {
 
 func TestUserCreateExists(t *testing.T) {
 	service := CreateStub()
+
 	request := CreateUserRequest{Username: "mksavic"}
 	service.Create(request)
+
 	_, err := service.Create(request)
 
 	if err == nil {
@@ -27,6 +39,7 @@ func TestUserCreateExists(t *testing.T) {
 
 func TestUserViewStandard(t *testing.T) {
 	service := CreateStub()
+
 	create_request := CreateUserRequest{Username: "mksavic"}
 	create_response, _ := service.Create(create_request)
 
@@ -40,6 +53,7 @@ func TestUserViewStandard(t *testing.T) {
 
 func TestUserViewDoesNotExist(t *testing.T) {
 	service := CreateStub()
+
 	request := ViewUserRequest{Uuid: "123456"}
 	_, err := service.View(request)
 
@@ -50,8 +64,9 @@ func TestUserViewDoesNotExist(t *testing.T) {
 
 func TestUserSearchStandard(t *testing.T) {
 	service := CreateStub()
+
 	create_request := CreateUserRequest{Username: "mksavic"}
-	service.Create(create_request)
+	create_response, _ := service.Create(create_request)
 
 	request := SearchUserRequest{Query: "sav"}
 	response, _ := service.Search(request)
@@ -59,10 +74,15 @@ func TestUserSearchStandard(t *testing.T) {
 	if len(response.Uuids) != 1 {
 		t.Fail()
 	}
+
+	if !contains(response.Uuids, create_response.Uuid) {
+		t.Fail()
+	}
 }
 
 func TestUserSearchDoesNotExist(t *testing.T) {
 	service := CreateStub()
+
 	request := SearchUserRequest{Query: "sav"}
 	response, _ := service.Search(request)
 
@@ -73,10 +93,13 @@ func TestUserSearchDoesNotExist(t *testing.T) {
 
 func TestUserSearchMulti(t *testing.T) {
 	service := CreateStub()
+
 	create_request := CreateUserRequest{Username: "mksavic"}
-	service.Create(create_request)
+	create_response_1, _ := service.Create(create_request)
+
 	create_request = CreateUserRequest{Username: "mds796"}
-	service.Create(create_request)
+	create_response_2, _ := service.Create(create_request)
+
 	create_request = CreateUserRequest{Username: "mvp307"}
 	service.Create(create_request)
 
@@ -84,6 +107,10 @@ func TestUserSearchMulti(t *testing.T) {
 	response, _ := service.Search(request)
 
 	if len(response.Uuids) != 2 {
+		t.Fail()
+	}
+
+	if !contains(response.Uuids, create_response_1.Uuid) || !contains(response.Uuids, create_response_2.Uuid) {
 		t.Fail()
 	}
 }
