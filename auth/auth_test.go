eab06f6fd83f71bd55e1c14374f56ed3a91b1b59
Mel Savich <msavich@umich.edu>
2018-11-08 18:53:11 -0500

    more auth verify/logout tests

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -108,3 +108,93 @@ func TestAuthVerifyBasic(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestAuthVerifyLoggedOut(t *testing.T) {
+	userService := user.CreateStub()
+	authService := CreateStub(userService)
+
+	registerRequest := RegisterAuthRequest{Username: "mksavic", Password: "abc123"}
+	registerResponse, _ := authService.Register(registerRequest)
+
+	logoutRequest := LogoutAuthRequest{Username: "mksavic"}
+	authService.Logout(logoutRequest)
+
+	verifyRequest := VerifyAuthRequest{Cookie: registerResponse.Cookie}
+	verifyResponse, err := authService.Verify(verifyRequest)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if verifyResponse.Username != "mksavic" {
+		t.Fail()
+	}
+}
+
+func TestAuthVerifyLogOutLogIn(t *testing.T) {
+	userService := user.CreateStub()
+	authService := CreateStub(userService)
+
+	registerRequest := RegisterAuthRequest{Username: "mksavic", Password: "abc123"}
+	authService.Register(registerRequest)
+
+	logoutRequest := LogoutAuthRequest{Username: "mksavic"}
+	authService.Logout(logoutRequest)
+
+	loginRequest := LoginAuthRequest{Username: "mksavic", Password: "abc123"}
+	loginResponse, _ := authService.Login(loginRequest)
+
+	verifyRequest := VerifyAuthRequest{Cookie: loginResponse.Cookie}
+	verifyResponse, err := authService.Verify(verifyRequest)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if verifyResponse.Username != "mksavic" {
+		t.Fail()
+	}
+}
+
+func TestAuthLogoutBasic(t *testing.T) {
+	userService := user.CreateStub()
+	authService := CreateStub(userService)
+
+	registerRequest := RegisterAuthRequest{Username: "mksavic", Password: "abc123"}
+	authService.Register(registerRequest)
+
+	logoutRequest := LogoutAuthRequest{Username: "mksavic"}
+	_, err := authService.Logout(logoutRequest)
+
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestAuthLogoutDoesNotExist(t *testing.T) {
+	userService := user.CreateStub()
+	authService := CreateStub(userService)
+
+	logoutRequest := LogoutAuthRequest{Username: "mksavic"}
+	_, err := authService.Logout(logoutRequest)
+
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestAuthLogoutLoggedOut(t *testing.T) {
+	userService := user.CreateStub()
+	authService := CreateStub(userService)
+
+	registerRequest := RegisterAuthRequest{Username: "mksavic", Password: "abc123"}
+	authService.Register(registerRequest)
+
+	logoutRequest := LogoutAuthRequest{Username: "mksavic"}
+	authService.Logout(logoutRequest)
+	_, err := authService.Logout(logoutRequest)
+
+	if err != nil {
+		t.Fail()
+	}
+}
