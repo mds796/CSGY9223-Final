9ded4856b934721591ce0d20749846c0e012a855
Mel Savich <mksavic@nyu.edu>
2018-11-30 17:33:37 -0500

    uuid --> uid

--- user/stub.go
+++ user/stub.go
@@ -10,13 +10,13 @@ import (
 const MIN_USERNAME = 6
 
 type StubService struct {
-	UuidCache     map[string]string // (UUID, username)
-	UsernameCache map[string]string // (username, UUID)
+	UIDCache      map[string]string // (UID, username)
+	UsernameCache map[string]string // (username, UID)
 }
 
 func CreateStub() *StubService {
 	stub := new(StubService)
-	stub.UuidCache = make(map[string]string)
+	stub.UIDCache = make(map[string]string)
 	stub.UsernameCache = make(map[string]string)
 	return stub
 }
@@ -32,15 +32,15 @@ func (s *StubService) Create(ctx context.Context, request *userpb.CreateUserRequ
 		return &userpb.CreateUserResponse{}, &CreateUserError{request.Username}
 	}
 
-	// generate the uuid
-	newUuid := uuid.New().String()
+	// generate the UID
+	newUID := uuid.New().String()
 
 	// add the user
-	s.UuidCache[newUuid] = request.Username
-	s.UsernameCache[request.Username] = newUuid
+	s.UIDCache[newUID] = request.Username
+	s.UsernameCache[request.Username] = newUID
 
 	// create the response
-	response := &userpb.CreateUserResponse{UID: newUuid}
+	response := &userpb.CreateUserResponse{UID: newUID}
 	return response, nil
 }
 
@@ -48,7 +48,7 @@ func (s *StubService) View(ctx context.Context, request *userpb.ViewUserRequest)
 	if id, ok := s.UsernameCache[request.Username]; ok {
 		// username exists
 		return &userpb.ViewUserResponse{UID: id, Username: request.Username}, nil
-	} else if username, ok := s.UuidCache[request.UID]; ok {
+	} else if username, ok := s.UIDCache[request.UID]; ok {
 		return &userpb.ViewUserResponse{UID: request.UID, Username: username}, nil
 	} else {
 		// username doesn't exist
@@ -57,7 +57,7 @@ func (s *StubService) View(ctx context.Context, request *userpb.ViewUserRequest)
 }
 
 func (s *StubService) Search(ctx context.Context, request *userpb.SearchUserRequest) (*userpb.SearchUserResponse, error) {
-	// find uuids that match given query
+	// find UIDs that match given query
 	var usernames []string
 	var userIds []string
 	for username, userId := range s.UsernameCache {
