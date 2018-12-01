c39c3c7e198153ba2768d237e3232c34fd0b11c7
Mel Savich <mksavic@nyu.edu>
2018-11-30 23:50:00 -0500

    actually running user and auth as a service now

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,1 +1,67 @@
 package cmd
+
+import (
+	"github.com/mds796/CSGY9223-Final/auth"
+	"github.com/spf13/cobra"
+)
+
+var authConfig auth.Config
+var authPidFile string
+
+func init() {
+	rootCmd.AddCommand(authCmd)
+
+	authCmd.PersistentFlags().StringVarP(&authPidFile, "pidFile", "p", ".auth.pid", "The name of the process ID file.")
+	authCmd.AddCommand(startAuthCmd)
+	authCmd.AddCommand(stopAuthCmd)
+	authCmd.AddCommand(restartAuthCmd)
+
+	authSetStartArgs(startAuthCmd)
+	authSetStartArgs(restartAuthCmd)
+}
+
+func authSetStartArgs(command *cobra.Command) {
+	command.Flags().StringVarP(&authConfig.Host, "host", "H", "localhost", "The host interface to listen on.")
+	command.Flags().Uint16VarP(&authConfig.Port, "port", "P", 8089, "The TCP port to listen on.")
+}
+
+var authCmd = &cobra.Command{
+	Use:   "auth",
+	Short: "Runs the auth server for our Twitter clone.",
+	Long:  `Runs a auth server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+var startAuthCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Starts the auth server for our Twitter clone.",
+	Long:  `Starts a auth server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		writePidFile(authPidFile)
+		err := auth.New(&authConfig).Start()
+
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+var stopAuthCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "Stops the auth server for our Twitter clone.",
+	Long:  `Stops a auth server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		stopServer(authPidFile)
+	},
+}
+
+var restartAuthCmd = &cobra.Command{
+	Use:   "restart",
+	Short: "Restarts the auth server for our Twitter clone.",
+	Long:  `Restarts a auth server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		stopAuthCmd.Run(cmd, args)
+		startAuthCmd.Run(cmd, args)
+	},
+}
--- cmd/user.go
+++ cmd/user.go
@@ -1,1 +1,67 @@
 package cmd
+
+import (
+	"github.com/mds796/CSGY9223-Final/user"
+	"github.com/spf13/cobra"
+)
+
+var userConfig user.Config
+var userPidFile string
+
+func init() {
+	rootCmd.AddCommand(userCmd)
+
+	userCmd.PersistentFlags().StringVarP(&userPidFile, "pidFile", "p", ".user.pid", "The name of the process ID file.")
+	userCmd.AddCommand(startUserCmd)
+	userCmd.AddCommand(stopUserCmd)
+	userCmd.AddCommand(restartUserCmd)
+
+	userSetStartArgs(startUserCmd)
+	userSetStartArgs(restartUserCmd)
+}
+
+func userSetStartArgs(command *cobra.Command) {
+	command.Flags().StringVarP(&userConfig.Host, "host", "H", "localhost", "The host interface to listen on.")
+	command.Flags().Uint16VarP(&userConfig.Port, "port", "P", 8088, "The TCP port to listen on.")
+}
+
+var userCmd = &cobra.Command{
+	Use:   "user",
+	Short: "Runs the user server for our Twitter clone.",
+	Long:  `Runs a user server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+var startUserCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Starts the user server for our Twitter clone.",
+	Long:  `Starts a user server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		writePidFile(userPidFile)
+		err := user.New(&userConfig).Start()
+
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+var stopUserCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "Stops the user server for our Twitter clone.",
+	Long:  `Stops a user server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		stopServer(userPidFile)
+	},
+}
+
+var restartUserCmd = &cobra.Command{
+	Use:   "restart",
+	Short: "Restarts the user server for our Twitter clone.",
+	Long:  `Restarts a user server process.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		stopUserCmd.Run(cmd, args)
+		startUserCmd.Run(cmd, args)
+	},
+}
