c21136766bfed37a2987e67a4720dcedff604b10
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2018-12-07 17:03:39 -0500

    Remove "stub" from post and use Raft

--- post/service.go
+++ post/service.go
@@ -6,22 +6,23 @@ import (
 	"github.com/google/uuid"
 	"github.com/mds796/CSGY9223-Final/post/postpb"
 	"github.com/mds796/CSGY9223-Final/storage"
+	"google.golang.org/grpc"
 	"time"
 )
 
-type StubService struct {
+type Service struct {
 	PostCache      storage.Storage
 	UserPostsCache storage.Storage
 }
 
-func CreateService(storageType storage.StorageType) *StubService {
-	stub := new(StubService)
+func CreateService(storageType storage.StorageType) *Service {
+	stub := new(Service)
 	stub.PostCache = storage.CreateStorage(storageType, "post/post_cache")
 	stub.UserPostsCache = storage.CreateStorage(storageType, "post/user_posts_cache")
 	return stub
 }
 
-func (stub *StubService) Create(ctx context.Context, request *postpb.CreateRequest) (*postpb.CreateResponse, error) {
+func (stub *Service) Create(ctx context.Context, request *postpb.CreateRequest) (*postpb.CreateResponse, error) {
 	if request.Post.Text == "" {
 		return nil, &EmptyPostTextError{Text: request.Post.Text}
 	}
@@ -54,7 +55,7 @@ func generateTimestamp() *postpb.Timestamp {
 	return &postpb.Timestamp{EpochNanoseconds: time.Now().UnixNano()}
 }
 
-func (stub *StubService) View(ctx context.Context, request *postpb.ViewRequest) (*postpb.ViewResponse, error) {
+func (stub *Service) View(ctx context.Context, request *postpb.ViewRequest) (*postpb.ViewResponse, error) {
 	post, err := stub.PostCache.Get(request.Post.ID)
 
 	if err != nil {
@@ -66,9 +67,25 @@ func (stub *StubService) View(ctx context.Context, request *postpb.ViewRequest) 
 	return &postpb.ViewResponse{Post: deserializedPost}, nil
 }
 
-func (stub *StubService) List(ctx context.Context, request *postpb.ListRequest) (*postpb.ListResponse, error) {
+func (stub *Service) List(ctx context.Context, request *postpb.ListRequest) (*postpb.ListResponse, error) {
 	posts, _ := stub.UserPostsCache.Get(request.User.ID)
 	deserializedPosts := &postpb.Posts{}
 	proto.Unmarshal(posts, deserializedPosts)
 	return &postpb.ListResponse{Posts: deserializedPosts.Posts}, nil
+}
+
+type StubClient struct {
+	service postpb.PostServer
+}
+
+func (s StubClient) Create(ctx context.Context, in *postpb.CreateRequest, opts ...grpc.CallOption) (*postpb.CreateResponse, error) {
+	return s.service.Create(ctx, in)
+}
+
+func (s StubClient) View(ctx context.Context, in *postpb.ViewRequest, opts ...grpc.CallOption) (*postpb.ViewResponse, error) {
+	return s.service.View(ctx, in)
+}
+
+func (s StubClient) List(ctx context.Context, in *postpb.ListRequest, opts ...grpc.CallOption) (*postpb.ListResponse, error) {
+	return s.service.List(ctx, in)
 }
